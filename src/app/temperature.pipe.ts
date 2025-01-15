d9c48b31068a3da0e124345454189f47f5a59f94
Gabriel_Martins_Padoin <gabriel.padoin@woli.com.br>
2025-01-14 21:06:04 -0300

    [NG] ACCEPTING_PARAMETERS_IN_CUSTOM_PIPES - feat: implementing the acceptance of parameters and processing of those parameters into legible values in the TemperaturePipe

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -6,8 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TemperaturePipe implements PipeTransform {
 
-  transform(value: string | number): any {
-    console.log('value: ', value)
+  transform(
+    value: string | number, 
+    inputType: 'cel' | 'fah', 
+    outputType?: 'cel' | 'fah'
+  ): any {
     let val: number;
 
     if (typeof value === 'string') {
@@ -16,9 +19,25 @@ export class TemperaturePipe implements PipeTransform {
       val = value
     }
 
-    const outputTemp = val * (9 / 5) + 32;
+    let outputTemp: number | undefined;
 
-    return `${outputTemp} °F`
+    if(inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if(inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9)
+    } else {
+      outputTemp = val
+    }
+
+    let symbol: '°C' | '°F'
+
+    if(!outputType) {
+      symbol = inputType === 'cel' ? '°C' : '°F'
+    } else {
+      symbol = outputType === 'cel' ? '°C' : '°F'
+    }
+
+    return `${outputTemp} ${symbol}`
   }
 
 }
