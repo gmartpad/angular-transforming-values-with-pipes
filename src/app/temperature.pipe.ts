ec9f7f63820ebe19c6547cc522f34a0a25a52b09
Gabriel_Martins_Padoin <gabriel.padoin@woli.com.br>
2025-01-14 21:25:29 -0300

    [NG] CHAINING_PIPES_AND_BEING_AWARE_OF_LIMITATIONS - feat/fix: adding the capability to receive null as value in the TransformPipe and using the toFixed method to present the temperature value in a nicer way

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -7,10 +7,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TemperaturePipe implements PipeTransform {
 
   transform(
-    value: string | number, 
+    value: string | number | null, 
     inputType: 'cel' | 'fah', 
     outputType?: 'cel' | 'fah'
   ): any {
+
+    if(!value) return value;
+
     let val: number;
 
     if (typeof value === 'string') {
@@ -37,7 +40,7 @@ export class TemperaturePipe implements PipeTransform {
       symbol = outputType === 'cel' ? '°C' : '°F'
     }
 
-    return `${outputTemp} ${symbol}`
+    return `${outputTemp.toFixed(2)} ${symbol}`
   }
 
 }
